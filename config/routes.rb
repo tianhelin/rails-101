8c90cd6f8c9328b5d8fc0d9153a4c7459972be06
Tianhe <tianhelin@hotmail.com>
2016-09-30 02:58:18 +0000

    Initialize the app

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+<span class="no">Rails</span><span class="p">.</span><span class="nf">application</span><span class="p">.</span><span class="nf">routes</span><span class="p">.</span><span class="nf">draw</span> <span class="k">do</span>
+  <span class="n">root</span> <span class="s1">'welcome#index'</span>
+<span class="k">end</span>
