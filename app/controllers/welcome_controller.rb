c1eee9ccbe293f385cc7db47b7bddf0d16e71d5d
Tianhe <tianhelin@hotmail.com>
2016-09-30 04:26:01 +0000

    Finished chpt 1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+
+    def index
+     flash[:warning] = "這是 warning 訊息！"    
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
-<span class="no">Rails</span><span class="p">.</span><span class="nf">application</span><span class="p">.</span><span class="nf">routes</span><span class="p">.</span><span class="nf">draw</span> <span class="k">do</span>
-  <span class="n">root</span> <span class="s1">'welcome#index'</span>
-<span class="k">end</span>
+Rails.application.routes.draw do
+  root 'welcome#index'
+  resources :welcome
+end
