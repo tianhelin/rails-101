a46bb6c30db5297e6c9e315d246ab4abb891b522
Tianhe <tianhelin@hotmail.com>
2016-09-30 10:37:39 +0000

    Finished Chapter 6.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,7 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-u.permit(:name, :email, :password, :password_confirmation)
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
-    end
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,11 +15,11 @@ class GroupsController < ApplicationController
     end
 
     def edit
-        @group = Group.find(params[:id])
+        @group = current_user.groups.find(params[:id])
     end
     
     def update
-       @group = Group.find(params[:id])
+       @group = current_user.groups.find(params[:id])
        if @group.update(group_params)
          redirect_to groups_path, notice: "修改討論版成功"
        else
@@ -28,7 +28,7 @@ class GroupsController < ApplicationController
     end
     
     def create
-      @group = Group.new(group_params)
+      @group = current_user.groups.find(params[:id])
       if @group.save
         redirect_to groups_path
       else
@@ -37,7 +37,7 @@ class GroupsController < ApplicationController
     end
     
     def destroy
-      @group = Group.find(params[:id])
+      @group = current_user.groups.find(params[:id])
       @group.destroy
       redirect_to groups_path, alert: "討論版已刪除"
     end
